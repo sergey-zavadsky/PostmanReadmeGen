b11b0668310970191a11ce76cca1857ef5ea3b41
Siarhei <siarhei.zavadski@gmail.com>
2023-10-29 15:22:43 +0100

    npm path fixes

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
 
-const directory = 'collections';
+// const directory = 'collections';
+export const generateTrees = (directory) => {
+	const processFilesRecursively = (dir) => {
+		const files = fs.readdirSync(dir);
 
-const processFilesRecursively = (dir) => {
-	const files = fs.readdirSync(dir);
+		files.forEach((file) => {
+			const filePath = path.join(dir, file);
+			if (fs.statSync(filePath).isDirectory()) {
+				processFilesRecursively(filePath);
+			} else if (file.endsWith('.postman_collection.json')) {
+				execSync(`node node_modules/postmanreadmegen/index.js "${filePath}"`, {
+					stdio: 'inherit',
+				});
+			}
+		});
+	};
 
-	files.forEach((file) => {
-		const filePath = path.join(dir, file);
-		if (fs.statSync(filePath).isDirectory()) {
-			processFilesRecursively(filePath);
-		} else if (file.endsWith('.postman_collection.json')) {
-			execSync(`node index.js "${filePath}"`, { stdio: 'inherit' });
-		}
-	});
+	processFilesRecursively(directory);
+	process.exit(0);
 };
-
-processFilesRecursively(directory);
-process.exit(0);
